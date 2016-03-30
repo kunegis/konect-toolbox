cbbff9cf2e216fda55eaf2424ee84e6243797ec2
Jérôme Kunegis <kunegis@gmail.com>
2016-03-30 16:00:53 +0000

    notice bug

--- m/konect_connect_strong_nobgl.m
+++ m/konect_connect_strong_nobgl.m
@@ -12,66 +12,71 @@ function [v] = konect_connect_strong_nobgl(a)
 
 [n,nx] = size(a);
 
-%whos
-
 a = logical(a ~= 0); 
 
-%whos
-
 count = 0; 
-count_max = 50; 
+count_max = 100; 
 
 v_best = [];
 v_best_size = 0; 
 
 while count < count_max
-  count = count + 1; 
+    count = count + 1; 
 
-  fprintf(1, '[%d]\n', count); 
+    fprintf(1, '[%d]\n', count); 
 
-  % Find an edge i→j
-  i = 1+floor(rand * n); 
-  ai = a(i,:); 
-  if sum(ai) == 0, continue; end;
-  js = find(ai);
-  j = js(1 + floor(rand * size(js,2))); 
+    % Find an edge i->j
+    i = 1+floor(rand * n); 
+    ai = a(i,:); 
+    if sum(ai) == 0, continue; end;
+    js = find(ai);
+    j = js(1 + floor(rand * size(js,2))); 
   
-  % Find all nodes reachable from j
-  u = zeros(n,1); 
-  u(j) = 1; 
-  k = 1; 
-  while 1
-    u = u | logical(a' * u); 
-    k_new = sum(u); 
-    fprintf(1, '  → %d\n', k_new); 
-    if k_new == k, break; end; 
-    k = k_new; 
-  end
+    % Find all nodes reachable from j
+    u = zeros(n,1); 
+    u(j) = 1; 
+    k = 1; 
+    while 1
+        u = u | logical(a' * u); 
+        k_new = sum(u); 
+        fprintf(1, '  ->%d\n', k_new); 
+        if k_new == k, break; end; 
+        k = k_new; 
+    end
 
-  % Find all nodes from which i is reachable
-  w = zeros(n,1); 
-  w(i) = 1; 
-  k = 1; 
-  while 1
-    w = w | logical(a * double(w)); 
-    k_new = sum(w); 
-    fprintf(1, '  ← %d\n', k_new); 
-    if k_new == k, break; end; 
-    k = k_new; 
-  end
+    % Find all nodes from which i is reachable
+    w = zeros(n,1); 
+    w(i) = 1; 
+    k = 1; 
+    while 1
+        w = w | logical(a * double(w)); 
+        k_new = sum(w); 
+        fprintf(1, '  <-%d\n', k_new); 
+        if k_new == k, break; end; 
+        k = k_new; 
+    end
 
-  % The intersection is part of the component
-  v = u & w; 
-  
-  fprintf(1, '  size = %d\n', sum(v)); 
+    % The intersection is part of the component
+    v = u & w; 
+    
+    fprintf(1, '  size = %d\n', sum(v)); 
 
-  v_size = sum(v); 
-  if v_size > v_best_size
-    v_best = v; 
-  end
+    v_size = sum(v); 
+    if v_size > v_best_size
+        v_best = v; 
+    end
 
-  % Cannot get bigger than this
-  if v_size > .5 * n, break; end; 
+    % Cannot get bigger than this
+    if v_size > .5 * n, break; end; 
 end
 
 v = v_best; 
+
+% The algorithm is imprecise, so if no component is found, just
+% return a single node as a component. 
+if sum(v) == 0
+    v(1) = 1;
+end
+
+% TODO eleminate impossible edges from the network continuously so
+% that the algorithm converges. 
