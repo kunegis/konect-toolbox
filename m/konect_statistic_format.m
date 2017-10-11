a34e1e2a6224997edae7ecdb8149db35f5aef438
Jérôme KUNEGIS <kunegis@gmail.com>
2017-10-11 15:45:51 +0200

    formal files for the format and weights statistics

--- m/konect_statistic_format.m
+++ m/konect_statistic_format.m
@@ -1,1 +1,8 @@
+%
+% The "format" as a statistic, i.e., the numerical ID of the format.
+%
 
+function values = konect_statistic_format(A, format, weights)
+
+  values(1) = format;
+  
---
+++ m/konect_statistic_weights.m
@@ -0,0 +1,8 @@
+%
+% The "weights" as a statistic, i.e., the numerical ID of the weights.
+%
+
+function values = konect_statistic_weights(A, format, weights)
+
+  values(1) = weights;
+  
