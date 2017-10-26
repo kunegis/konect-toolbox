c63412fc127440bf51a8fe8c562c095918711623
Jérôme Kunegis <kunegis@gmail.com>
2017-10-26 15:03:19 +0200

    update KONECT URL

--- m/private/konect_spectral_distribution_plain.m
+++ m/private/konect_spectral_distribution_plain.m
@@ -10,8 +10,8 @@
 %
 % ABOUT 
 %	This file is part of the KONECT Matlab Toolbox version 0.3.
-%	konect.uni-koblenz.de
-%	(c) Jerome Kunegis 2014; this is Free Software released under
+%	konect.cc
+%	(c) Jerome Kunegis 2017; this is Free Software released under
 %	the GPLv3, see COPYING. 
 %
 
