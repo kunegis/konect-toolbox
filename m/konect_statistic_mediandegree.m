4224375503a212f2ed235874fd05e136171e7c86
Jérôme KUNEGIS <kunegis@gmail.com>
2018-03-01 09:59:27 +0100

    median degree (statistic)

---
+++ m/konect_statistic_mediandegree.m
@@ -0,0 +1,20 @@
+%
+% The median degree.
+%
+% values
+%	[1] 	Median degree
+%	[2]	Left median degree (BIP)
+%	[3]	Right median degree (BIP)
+%	[4]	Median outdegree (ASYM)
+%	[5]	Median indegree (ASYM)
+%
+% GROUP+2:  bip
+% GROUP+3:  bip
+% GROUP+4:  asym
+% GROUP+5:  asym
+%
+
+error % Not implemented in Matlab; only in C in konect-analysis/
+
+
+
